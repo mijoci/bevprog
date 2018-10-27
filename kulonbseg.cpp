86011296d5af23fdb93dba6e65a4867b7f0dea76
mijoci <43492551+mijoci@users.noreply.github.com>
2018-10-27 11:19:59 +0200

    Add files via upload

---
+++ kulonbseg.cpp
@@ -0,0 +1,14 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+int a;
+int b;
+cout<<"adjon meg két számot!\n";
+cin>>a>>b;
+a = a+b;
+b = a-b;
+a = a-b;
+
+cout<<"a = "<<a<<" b = "<<b<<'\n';
+}
